e0144291f0a7529fc2e67608800a2a5890c7e2af
Vlad Kulagin <vladkulagin0801@gmail.com>
2021-12-07 02:18:03 +0300

    Refact

--- js/script.js
+++ js/script.js
@@ -59,6 +59,7 @@ let x = '';
 let y = '';
 let action = '';
 let result = '';
+let end = false;
 
 
 let arrSign = ['+', '-', '/', '*', '=', 'AC', '%', '+/-'];
@@ -93,6 +94,7 @@ const calculate = () => {
 
   info.textContent = result;
 
+  end = true;
   x = result;
   y = '';
 };
@@ -104,6 +106,24 @@ const clearAll = () => {
   result = '';
 };
 
+const plusMinus = () => {
+  if (action === '-') {
+    action = '+';
+    return info.textContent = action;
+  } else if (action === '+') {
+    action = '-';
+    return info.textContent = action;
+  } else if (x.split('').includes('-')) {
+    x = `+${x.slice(1)}`;
+    return info.textContent = x;
+  } else if (x.split('').includes('+')) {
+    x = `-${x.slice(1)}`;
+    return info.textContent = x;
+  } else {
+    x = `-${x}`;
+    return info.textContent = x;
+  }
+};
 
 document.querySelector('.btns').onclick = (e) => {
 
@@ -113,46 +133,16 @@ document.querySelector('.btns').onclick = (e) => {
   const click = e.target.textContent;
   const signName = arrSign.includes(click);
 
-  if (click !== '=') info.textContent += click;
-
-
-  if (result && !signName) {
-    clearAll();
-    x = click;
-    return info.textContent = click;
-  } else if (result === '∞') {
-    clearAll();
-    x = click;
-    return info.textContent = click;
-  } else if (result && signName) {
-    x = result;
-    action = click;
-    result = '';
+  if (click !== '=') {
+    info.textContent += click;
   }
+
 
   if (click === 'AC') {
     clearAll();
     return info.textContent = '';
   }
 
-  if (click === '+/-') {
-    if (action === '-') {
-      action = '+';
-      return info.textContent = action;
-    } else if (action === '+') {
-      action = '-';
-      return info.textContent = action;
-    } else if (x.split('').includes('-')) {
-      x = `+${x.slice(1)}`
-      return info.textContent = x;
-    }else if (x.split('').includes('+')) {
-      x = `-${x.slice(1)}`
-      return info.textContent = x;
-    } else {
-      x = `-${x}`
-      return info.textContent = x;
-    }
-  }
 
   if (click === '%' && y === '') {
     x = x / 100;
@@ -163,18 +153,45 @@ document.querySelector('.btns').onclick = (e) => {
     return info.textContent = y;
   }
 
+
+  if (click === '+/-') {
+    plusMinus()
+  }
+
+
+  if (end && action === '=') {
+    if (signName && click !== '=') {
+      action = click;
+      end = false;
+      return info.textContent = action;
+    } else {
+      clearAll();
+      x = click;
+      end = false;
+      return info.textContent = x;
+    }
+  } else if (result === '∞') {
+    clearAll();
+    x = click;
+    return info.textContent = x;
+  }
+
+
   if (!y && click === '=') {
-    return null;
+    y = +x;
+    info.textContent = y;
   }
 
   if (x && signName) {
     y && calculate();
     action = click;
   } else if (!action) x += click;
-  else if (action) y += click;
+  else if (action) {
+    y += click;
+    info.textContent = y;
+  }
 
-  console.log('x', x[0]);
+  console.log('x', x);
   console.log('y', y);
   console.log('action', action);
 };
-
